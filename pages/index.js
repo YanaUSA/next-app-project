8148b85b9e02573c4f4bc382b9a426e93c5ef399
YanaUSA <usatiukiana@gmail.com>
2023-06-22 12:04:21 +0300

    add dynamic routes

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function Home() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Products</Link>
+      <Link href="/product">Products</Link>
+    </>
+  );
+}
+export default Home;
---
+++ pages/product/index.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+function ProductList({ productId }) {
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <Link href="/product/1">
+        <h2>Product 1</h2>
+      </Link>
+      <Link href="/product/2" replace>
+        <h2>Product 2</h2>
+      </Link>
+      <Link href={`/product/${productId}`}>
+        <h2>Product {productId}</h2>
+      </Link>
+    </>
+  );
+}
+
+export default ProductList;
